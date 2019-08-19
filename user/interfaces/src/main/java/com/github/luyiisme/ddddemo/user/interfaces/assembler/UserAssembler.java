1fa69cf3bfdf5df16a5066e64ddc1c656d581087
kevin.luy <kevin.luy@alibaba-inc.com>
2019-08-19 21:41:04 +0800

    refactor to bean copier

--- user/interfaces/src/main/java/com/github/luyiisme/ddddemo/user/interfaces/assembler/UserAssembler.java
+++ user/interfaces/src/main/java/com/github/luyiisme/ddddemo/user/interfaces/assembler/UserAssembler.java
@@ -2,20 +2,22 @@ package com.github.luyiisme.ddddemo.user.interfaces.assembler;
 
 import com.github.luyiisme.ddddemo.user.domain.User;
 import com.github.luyiisme.ddddemo.user.interfaces.facade.dto.UserDTO;
+import org.springframework.cglib.beans.BeanCopier;
 
 /**
  * @author: kevin.luy@antfin.com
  * @create: 2019-08-19 15:03
  **/
 public final class UserAssembler {
+    static final BeanCopier beanCopier = BeanCopier.create(User.class, UserDTO.class, false);
+
 
     public static UserDTO toDTO(User user) {
         if (user == null) {
             return null;
         }
         UserDTO dto = new UserDTO();
-        dto.setId(user.getId());
-        dto.setName(user.getName());
+        beanCopier.copy(user, dto, null);
         return dto;
     }
 }
